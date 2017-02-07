700cb813cb13b625ef35eb67bdc7ed70b317f336
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-07 02:22:15 -0800

    make homepage groups

--- wordpress-data/wp-content/themes/currentlykira/index.php
+++ wordpress-data/wp-content/themes/currentlykira/index.php
@@ -1,10 +1,23 @@
 <?php get_header(); ?>
-<section id="content" role="main">
+
+<?php $count = 0; ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
+  <?php if ($count == 0): ?>
+    <?php get_template_part( 'entry', '1' ); ?>
+    <?php comments_template(); ?>
+  <?php endif ?>
+  <?php if ($count == 1 || $count == 2): ?>
+    <?php get_template_part( 'entry', '2' ); ?>
+    <?php comments_template(); ?>
+  <?php endif ?>
+  <?php if ($count > 2): ?>
+    <?php get_template_part( 'entry', '3' ); ?>
+    <?php comments_template(); ?>
+  <?php endif ?>
+<?php $count = $count + 1; ?>
 <?php endwhile; endif; ?>
+
 <?php get_template_part( 'nav', 'below' ); ?>
-</section>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
