b3cdc163b48dd2be4045718395fe54384f7f1cb4
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-07 18:49:20 -0800

    Set Post Image for top row

--- wordpress-data/wp-content/themes/currentlykira/entry-1.php
+++ wordpress-data/wp-content/themes/currentlykira/entry-1.php
@@ -1,13 +1,30 @@
 <!-- entry-1 -->
 <div class="row">
   <div class="column">
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header>
-    <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
-    <?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
-    </header>
-    <?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
-    <?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry-1' ); ?>>
+      <div class="img-container" style="background-image: url(<?php the_post_thumbnail_url(large); ?>)">
+        <article class="post-info">
+          <?php if ( is_singular() ) {
+            echo '<h1 class="entry-title">';
+          } else {
+            echo '<h2 class="entry-title">';
+          } ?>
+          <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
+            <?php the_title(); ?>
+          </a>
+          <?php if ( is_singular() ) {
+            echo '</h1>';
+          } else {
+            echo '</h2>';
+          } ?>
+        </article>
+      </div>
+      <header>
+        <?php edit_post_link(); ?>
+        <?#php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
+      </header>
+      <?#php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
+      <?#php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
     </article>
   </div>
 </div>
