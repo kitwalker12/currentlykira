5335640e25563fdb009846c829b24a0ab1e93210
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-08 23:07:48 -0800

    style category page

--- wordpress-data/wp-content/themes/currentlykira/category.php
+++ wordpress-data/wp-content/themes/currentlykira/category.php
@@ -1,15 +1,30 @@
 <!-- category -->
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Category Archives: ', 'currentlykira' ); ?><?php single_cat_title(); ?></h1>
-<?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
-</header>
+
+<?php $count = 0; ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
+  <?php if ($count == 0): ?>
+    <?php get_template_part( 'entry', '1' ); ?>
+    <?php comments_template(); ?>
+  <?php endif ?>
+  <?php if ($count == 1): ?>
+    <div class="row">
+      <div class="column">
+        <h2 class="entry-latest-posts">
+          ALL IN <span class="category-title"><?php single_cat_title(); ?></span>
+        </h2>
+      </div>
+    </div>
+  <?php endif ?>
+  <?php if ($count > 0): ?>
+    <?php get_template_part( 'entry', 'cat3' ); ?>
+    <?php comments_template(); ?>
+  <?php endif ?>
+<?php $count = $count + 1; ?>
 <?php endwhile; endif; ?>
+
 <?php get_template_part( 'nav', 'below' ); ?>
-</section>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
 <!-- END category -->
