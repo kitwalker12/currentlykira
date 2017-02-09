614e45752f5939f56b559f2220e3898c2311cfec
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-09 00:03:06 -0800

    Post Styling

--- wordpress-data/wp-content/themes/currentlykira/entry.php
+++ wordpress-data/wp-content/themes/currentlykira/entry.php
@@ -1,10 +1,32 @@
 <!-- entry -->
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header>
-<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
-<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
-</header>
-<?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
-<?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
-</article>
+<header><?php edit_post_link(); ?></header>
+<div class="row">
+  <div class="column">
+    <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
+      <div class="img-container" style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)">
+      </div>
+      <article class="entry-info">
+        <h3 class="entry-hashtag">
+          <?php if ( get_post_meta( get_the_ID(), 'hashtag', true ) ): ?>
+            <?php echo get_post_meta( get_the_ID(), 'hashtag', true ); ?>
+          <?php else: ?>
+            <?php echo "#CURRENTLYBEAUTY"; ?>
+          <?php endif ?>
+        </h3>
+        <h1 class="entry-title">
+          <?php the_title(); ?>
+        </h1>
+        <p class="entry-tagline">
+          <?php the_time( get_option( 'date_format' ) ); ?>
+        </p>
+        <div class="entry-line">&nbsp;</div>
+      </article>
+    </article>
+  </div>
+</div>
+<div class="row">
+  <div class="column">
+    <?php get_template_part( 'entry', 'content' ); ?>
+  </div>
+</div>
 <!-- END entry -->
