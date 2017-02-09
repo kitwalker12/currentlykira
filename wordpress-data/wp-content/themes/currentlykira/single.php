d8c0fc562563c9d2a1d93836487a8a2501f48a32
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-09 00:18:06 -0800

    Post Styling

--- wordpress-data/wp-content/themes/currentlykira/single.php
+++ wordpress-data/wp-content/themes/currentlykira/single.php
@@ -1,6 +1,6 @@
 <!-- single -->
 <?php get_header(); ?>
-<section id="content" role="main">
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
@@ -8,7 +8,7 @@
 <footer class="footer">
 <?php get_template_part( 'nav', 'below-single' ); ?>
 </footer>
-</section>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
 <!-- END single -->
