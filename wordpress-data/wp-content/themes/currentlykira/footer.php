182912ce2c226dfa22ee7a15764832ef22e12eae
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-08 17:59:35 -0800

    setup footer

--- wordpress-data/wp-content/themes/currentlykira/footer.php
+++ wordpress-data/wp-content/themes/currentlykira/footer.php
@@ -2,7 +2,26 @@
   </section>
   <footer class="footer">
     <section class="container">
-      <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'currentlykira' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'currentlykira' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+      <div class="row">
+        <div class="column column-25">
+          <div class="footer-title">
+            <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+          </div>
+        </div>
+        <div class="column column-75">
+          <div class="footer-nav">
+            <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => false, 'menu_class' => 'float-right' ) ); ?>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column column-50">
+          <?php echo sprintf( __( 'all rights reserved. %1$s %2$s', 'currentlykira' ), esc_html( get_bloginfo( 'name' ) ), date( 'Y' ) ); ?>
+        </div>
+        <div class="column column-50 footer-attrib">
+          <?php echo sprintf( __( ' Site Designed By: %1$s.', 'currentlykira' ), '<a href="http://www.rosalie.agency">Rosalie</a>' ); ?>
+        </div>
+      </div>
     </section>
   </footer>
 </main>
