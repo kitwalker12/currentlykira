75d7ae2275d4798256ac7daaa9fb392d49523991
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-10 00:19:11 -0800

    disable pagination for now

--- wordpress-data/wp-content/themes/currentlykira/nav-below-single.php
+++ wordpress-data/wp-content/themes/currentlykira/nav-below-single.php
@@ -1,6 +1,6 @@
 <!-- nav-below-single -->
-<nav id="nav-below" class="navigation" role="navigation">
-<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
-<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
+<!-- <nav id="nav-below" class="navigation" role="navigation">
+<div class="nav-previous"><?#php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
+<div class="nav-next"><?#php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
 </nav>
-<!-- END nav-below-single -->
+ --><!-- END nav-below-single -->
