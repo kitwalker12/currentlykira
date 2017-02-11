1e3d21852debb1f78a6a4f70163c6987765e0854
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-11 00:42:12 -0800

    disable comments

--- wordpress-data/wp-content/themes/currentlykira/page.php
+++ wordpress-data/wp-content/themes/currentlykira/page.php
@@ -2,8 +2,8 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry', 'page' ); ?>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+  <?php get_template_part( 'entry', 'page' ); ?>
+  <?#php if ( ! post_password_required() ) comments_template( '', true ); ?>
 <?php endwhile; endif; ?>
 
 <?php get_sidebar(); ?>
