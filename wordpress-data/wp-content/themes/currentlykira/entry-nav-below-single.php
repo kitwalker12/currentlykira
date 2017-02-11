8448101b5257c2193a633cd1a5c867b222c921f6
Aditya Raghuwanshi <adi.version1@gmail.com>
2017-02-10 18:26:42 -0800

    nav below posts

---
+++ wordpress-data/wp-content/themes/currentlykira/entry-nav-below-single.php
@@ -0,0 +1,51 @@
+<!-- entry-2 -->
+<?php if ($wp_query->current_post == 1) {
+  echo "<div class='row'>";
+} ?>
+<div class="column column-50">
+  <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry-2' ); ?>>
+    <div class="img-container" style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)">
+      <article class="entry-info">
+        <h3 class="entry-hashtag">
+          <?php if ( get_post_meta( get_the_ID(), 'hashtag', true ) ): ?>
+            <?php echo get_post_meta( get_the_ID(), 'hashtag', true ); ?>
+          <?php else: ?>
+            <?php echo "#CURRENTLYBEAUTY"; ?>
+          <?php endif ?>
+        </h3>
+        <?php if ( is_singular() ) {
+          echo '<h1 class="entry-title">';
+        } else {
+          echo '<h2 class="entry-title">';
+        } ?>
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
+          <?php the_title(); ?>
+        </a>
+        <?php if ( is_singular() ) {
+          echo '</h1>';
+        } else {
+          echo '</h2>';
+        } ?>
+        <div class="entry-line">&nbsp;</div>
+        <?php if ( get_post_meta( get_the_ID(), 'tagline', true ) ): ?>
+          <p class="entry-tagline">
+            <?php echo get_post_meta( get_the_ID(), 'tagline', true ); ?>
+          </p>
+        <?php endif ?>
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="entry-read-more">
+          READ MORE
+        </a>
+      </article>
+    </div>
+    <header>
+      <?php edit_post_link(); ?>
+      <?#php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
+    </header>
+    <?#php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
+    <?#php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
+  </article>
+</div>
+<?php if (($wp_query->current_post + 1) == ($wp_query->post_count) || ($wp_query->current_post == 2)) {
+  echo "</div>";
+} ?>
+<!-- END entry-2 -->
--- wordpress-data/wp-content/themes/currentlykira/nav-below-single.php
+++ wordpress-data/wp-content/themes/currentlykira/nav-below-single.php
@@ -1,6 +1,15 @@
 <!-- nav-below-single -->
-<!-- <nav id="nav-below" class="navigation" role="navigation">
-<div class="nav-previous"><?#php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
-<div class="nav-next"><?#php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
-</nav>
- --><!-- END nav-below-single -->
+<section class="row">
+  <?php
+    $nav_below_posts = get_posts(array(
+      'numberposts' => 2,
+      'exclude' => array(get_the_ID())
+    ))
+  ?>
+  <?php $custom_query = new WP_Query(array( 'posts_per_page' => 2, 'post__not_in' => array( get_the_ID() ) ));
+    while($custom_query->have_posts()) : $custom_query->the_post(); ?>
+      <?php get_template_part( 'entry', 'nav-below-single' ); ?>
+    <?php endwhile; ?>
+  <?php wp_reset_postdata(); ?>
+</section>
+<!-- END nav-below-single -->
